04291949e921ba6eb86977627285d4becf442a7e
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-08-15 00:18:38 +0700

    init

--- src/background.js
+++ src/background.js
@@ -6,6 +6,15 @@ chrome.bookmarks.onCreated.addListener(() => {
     alert('Bookmard saved');
 });
 
+/** Works only on disabled default_popup in manifest */
+chrome.action.onClicked.addListener((tab) => {
+    console.log({ tab });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['contentScript.js']
+    });
+});
+
 /** From content to background */
 chrome.runtime.onMessage.addListener((request, sender, response) => {
     console.log(request, sender);
